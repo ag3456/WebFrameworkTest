d8c6dff395e8160b313616d2d3e104e688a7e506
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2018-08-23 14:58:37 -0400

    pep8

--- bucsp/readh5file.py
+++ bucsp/readh5file.py
@@ -11,41 +11,44 @@ d = {}
 groups = []
 p = {}
 datasets = []
-def print_hdf5_file_structure(file_name) :
+
+
+def print_hdf5_file_structure(file_name):
     """Prints the HDF5 file structure"""
     with h5py.File(file_name, 'r') as f:
-        item = f #["/Configure:0000/Run:0000"]
+        item = f  # ["/Configure:0000/Run:0000"]
         print_hdf5_item_structure(item)
 
- 
-def print_hdf5_item_structure(g, offset='    ') :
+
+def print_hdf5_item_structure(g, offset='    '):
     """Prints the input file/group/dataset (g) name and begin iterations on its content"""
-    if isinstance(g,h5py.File):
-        print (g.file, g.name)
-        #x.extend(g.file)
-    elif isinstance(g,h5py.Dataset) and not isinstance(g,h5py.Group):
-        #, g.dtype
+    if isinstance(g, h5py.File):
+        print(g.file, g.name)
+        # x.extend(g.file)
+    elif isinstance(g, h5py.Dataset) and not isinstance(g, h5py.Group):
+        # , g.dtype
         d['label'] = g.name.rsplit("/", 1)[1]
-        d['value'] =g.name.rsplit("/", 1)[1]
+        d['value'] = g.name.rsplit("/", 1)[1]
         groups.append(d.copy())
-    elif isinstance(g,h5py.Group):
+    elif isinstance(g, h5py.Group):
         if ("Parameters" in g.name.rsplit("/", 1)[1]):
             p['label'] = g.name.rsplit("/", 2)[1] + "/" + g.name.rsplit("/", 1)[1]
             p['value'] = g.name
             datasets.append(p.copy())
     else:
         raise KeyError('UNKNOWN ITEM IN HDF5 FILE')
- 
-    if isinstance(g, h5py.File) or isinstance(g, h5py.Group) :
-        for key,val in dict(g).items() :
+
+    if isinstance(g, h5py.File) or isinstance(g, h5py.Group):
+        for key, val in dict(g).items():
             subg = val
-            #print (offset, key) #,"   ", subg.name #, val, subg.len(), type(subg),
+            # print (offset, key) #,"   ", subg.name #, val, subg.len(), type(subg),
             print_hdf5_item_structure(subg, offset + '    ')
-            
-def return_options(file_name):  #this is done to return the options variable 
+
+
+def return_options(file_name):  # this is done to return the options variable
     print_hdf5_file_structure(file_name)
     return groups, datasets
- 
-#if __name__ == "__main__" :
-#groups, datasets = return_options('/home/ashaki/Downloads/mlh170821i.004.hdf5')
+
+# if __name__ == "__main__" :
+#    groups, datasets = return_options('/home/ashaki/Downloads/mlh170821i.004.hdf5')
 #    print( "End of test" )
--- bucsp/returningdata2.py
+++ bucsp/returningdata2.py
@@ -1,34 +1,33 @@
-import numpy as np   
-import os
 import h5py
 from datetime import datetime
-from typing import List, Tuple
+from typing import Tuple
 from pathlib import Path
 
 group = '/Data/Array Layout/Array with kinst=32 and mdtyp=115 and pl=0.00048 /2D Parameters'
-groupsplit = group.rsplit("/" , 1)[0]
+groupsplit = group.rsplit("/", 1)[0]
 dataset = 'ne'
 field = group + '/' + dataset
 
-#fileapi = '/Data/Array Layout/Array with kinst=32 and mdtyp=115 and pl=0.00048 /2D Parameters/ne'
+# fileapi = '/Data/Array Layout/Array with kinst=32 and mdtyp=115 and pl=0.00048 /2D Parameters/ne'
+
+
 def returningdata2(filename: Path, fileapi,
-				   rnglim: Tuple[float, float]=None, 
-				   tlim: Tuple[datetime, datetime]=None):
+                   rnglim: Tuple[float, float]=None,
+                   tlim: Tuple[datetime, datetime]=None):
 
     filename = Path(filename).expanduser()
 
-#filename = '/Users/ashakigumbs/Documents/Research_Semeter_Group/madmatlab/Zenith+single-pulse+basic+parameters.hdf5'
+# filename = '/Users/ashakigumbs/Documents/Research_Semeter_Group/madmatlab/Zenith+single-pulse+basic+parameters.hdf5'
 
-     
-    with h5py.File(filename, 'r') as f: 
+    with h5py.File(filename, 'r') as f:
         tandrng = fileapi.rsplit("/", 2)[0]
         rng = f[tandrng + '/range'][:]
         tut = f[tandrng + '/timestamps'][:]
         data = f[fileapi][:]
         newtime = [datetime.utcfromtimestamp(t) for t in tut]
-        
 
     if rnglim is None and tlim is None:
         return newtime, rng, data
+
 
 newtime, rng, data = returningdata2('~/Downloads/mlh170821i.004.hdf5', field)
